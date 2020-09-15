cf15f2a724ac3ab4c34353583c11ed7353cc9538
Maciej Witkowski <maciekwitkowski322@gmail.com>
2020-09-15 18:17:15 +0200

    Cosmetics

--- client/src/queries/fetchGame.js
+++ client/src/queries/fetchGame.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export default gql`
-    query FetchGame($id: String){
+    query FetchGame($id: String!){
 	    game(id: $id) {
             name,
             description,
